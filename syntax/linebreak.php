3afc7a5504acd212cd76cef7a9dd4ea139ad60cd
Michael Klier <chi@chimeric.de>
2008-06-07 22:43:00 +0200

    fixed code style
    darcs-hash:20080607204300-23886-de335f85eca78fde25a5782e9aaa7e9e6fba6683.gz

--- syntax/linebreak.php
+++ syntax/linebreak.php
@@ -7,53 +7,54 @@
  * @author     Christopher Smith <chris@jalakai.co.uk>
  * @author     Esther Brunner <wikidesign@gmail.com>
  */
- 
+
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
- 
+
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
- 
+
 /**
  * All DokuWiki plugins to extend the parser/rendering mechanism
  * need to inherit from this class
  */
 class syntax_plugin_creole_linebreak extends DokuWiki_Syntax_Plugin {
- 
-  function getInfo(){
-    return array(
-      'author' => 'Gina Häußge, Michael Klier, Christopher Smith',
-      'email'  => 'dokuwiki@chimeric.de',
-      'date'   => '2008-02-12',
-      'name'   => 'Creole Plugin (linebreak component)',
-      'desc'   => 'Provide a line break for a new line in the raw wiki data',
-      'url'    => 'http://wiki.splitbrain.org/plugin:creole',
-    );
-  }
 
-  function getType() { return 'substition'; }
-  function getSort() { return 100; }
-  
-  function connectTo($mode) {
-    $this->Lexer->addSpecialPattern(
-      '(?<!^|\n)\n(?!\n|>)',
-      $mode,
-      'plugin_creole_linebreak'
-    ); 
-  }
+	function getInfo() {
+		return array(
+				'author' => 'Gina Häußge, Michael Klier, Christopher Smith',
+				'email'  => 'dokuwiki@chimeric.de',
+				'date'   => '2008-02-12',
+				'name'   => 'Creole Plugin (linebreak component)',
+				'desc'   => 'Provide a line break for a new line in the raw wiki data',
+				'url'    => 'http://wiki.splitbrain.org/plugin:creole',
+				);
+	}
 
-  function handle($match, $state, $pos, &$handler){ 
+	function getType() { return 'substition'; }
+	function getSort() { return 100; }
 
-    if ($match == "\n") return true;
-    return false;
-  }
+	function connectTo($mode) {
+		$this->Lexer->addSpecialPattern(
+				'(?<!^|\n)\n(?!\n|>)',
+				$mode,
+				'plugin_creole_linebreak'
+				); 
+	}
 
-  function render($mode, &$renderer, $data){
-  
-    if($mode == 'xhtml'){
-        if ($data) $renderer->doc .= "<br />";
-        return true;
-    }
-    return false;
-  }
+	function handle($match, $state, $pos, &$handler) { 
+
+		if ($match == "\n") return true;
+		return false;
+	}
+
+	function render($mode, &$renderer, $data) {
+
+		if($mode == 'xhtml') {
+			if ($data) $renderer->doc .= "<br />";
+			return true;
+		}
+		return false;
+	}
 }
+//vim:ts=4:sw=4:et:enc=utf-8:
