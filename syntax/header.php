3f589994d717755d3acd3df7c1a8515aab60ab34
LarsDW223 <lars_paulsen@web.de>
2015-08-08 18:19:40 +0200

    Deleted specific header level calculation for config option 'precedence' beeing set to 'DokuWiki'. Fixes #8.

--- syntax/header.php
+++ syntax/header.php
@@ -46,16 +46,9 @@ class syntax_plugin_creole_header extends DokuWiki_Syntax_Plugin {
     }
 
     function handle($match, $state, $pos, Doku_Handler $handler) {
-        global $conf;
-
         // get level and title
         $title = trim($match);
-        if (($this->getConf('precedence') == 'dokuwiki')
-                && ($title{strlen($title) - 1} == '=')) { // DokuWiki
-            $level = 7 - strspn($title, '=');
-        } else {                                   // Creole
-            $level = strspn($title, '=');
-        }
+        $level = strspn($title, '=');
         if ($level < 1) $level = 1;
         elseif ($level > 5) $level = 5;
         $title = trim($title, '=');
